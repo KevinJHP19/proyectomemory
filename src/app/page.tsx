4b46bb1913f463246f44978f21357768318aa6c5
KevinJHP19 <kevinjordanhuaman@gmail.com>
2025-04-11 16:10:37 +0200

    instalamos shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from "next/image";
+
+import Index from "./pages/Index";
 
 export default function Home() {
   return (
     <div>
       <h1>Hola nuevo proyecto memory</h1>
+      <Index/>
     </div>
   );
 }
