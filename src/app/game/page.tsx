3cf53429095b7bbffb270f3099c7b398355e5624
KevinJHP19 <kevinjordanhuaman@gmail.com>
2025-05-06 17:15:18 +0200

    crear un contador individual para cada tarjeta

--- src/app/game/page.tsx
+++ src/app/game/page.tsx
@@ -7,13 +7,13 @@ export default function Game(){
       <h1 className="text-2xl font-bold">Página de Juego</h1>
       <p>Contenido de la página de juego.</p>
       
-<div className="grid grid-cols-5 grid-rows-5 gap-4">
+<div className="grid grid-cols-5 grid-rows-5 gap-4 border-2 border-neutral-950 ">
     <div className="col-span-1 bg-sky-300 border-2 border-sky-500 rounded-lg flex flex-col justify-center items-center gap-4 pt-[250px] pb-[250px]">
         <h1 className="font-bold">MEMORY</h1>
 
         <div className="flex flex-col items-center gap-2">
             <span>Tiempo:</span>
-            <div id="tiempo" className="p-[50px] flex justify-center items-center bg-cyan-900 text-white rounded-lg p-2 border-2 border-red-700">
+            <div id="tiempo" className=" flex justify-center items-center bg-cyan-900 text-white rounded-lg p-2 border-2 border-red-700">
                 <span>0</span>
             </div>
         </div>
--- src/miscomponentes/Tarjeta.tsx
+++ src/miscomponentes/Tarjeta.tsx
@@ -1,9 +1,12 @@
+'use client'
+import React, { useState } from "react";
 import {
     Card,
     CardContent,
     
     CardHeader,
     CardTitle,
+    CardFooter,
 
 } from "@/components/ui/card"
  
@@ -14,17 +17,33 @@ interface TarjetaProps {
 }
 
 export const Tarjeta = ({nombre, imagen}: TarjetaProps) =>{
-    return (
-        <Card className="bg-[#F2EBCB]">
-        <CardHeader>
-            <CardTitle>{nombre}</CardTitle>
-            
-        </CardHeader>
-        <CardContent>
-            <img src={imagen} alt={nombre}  className="w-[150px] h-[150px]"/>
-        </CardContent>
+
+    const [clicktarjeta, setClicktarjeta] = useState(0);
+
+    function Contadortarjeta(){
+        setClicktarjeta(clicktarjeta + 1);
+        console.log("Contador de tarjeta: " + clicktarjeta);
         
-        </Card>
+
+    }
+    return (
+        <a onClick={Contadortarjeta}  className="flex justify-center items-center">
+            <Card className="bg-[#F2EBCB]">
+            <CardHeader>
+                <CardTitle>{nombre}</CardTitle>
+                
+            </CardHeader>
+            <CardContent>
+                <img src={imagen} alt={nombre}  className="w-[150px] h-[150px]"/>
+            </CardContent>
+            <CardFooter className="flex justify-center items-center">
+                <div className="border-2 border-red-700 p-[10px]">
+                    <p>Clicks : <span>{clicktarjeta}</span></p>
+                </div>
+            </CardFooter>
+            
+            </Card>
+        </a>
 
     );
 }
